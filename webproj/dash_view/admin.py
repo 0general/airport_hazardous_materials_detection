67fdd5f81dcece7b363df1c60d67e4b06c537fb2
gigascake <gigascake@gmail.com>
2022-04-30 01:52:43 +0900

    버그수정

--- webproj/dash_view/admin.py
+++ webproj/dash_view/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-from dash_view.models import DetectedItem
-
-
-class DetectedItemAdmin(admin.ModelAdmin):
-    list_display = ('id', 'item', 'timestamp', 'confidence', 'filename')
-
-
-admin.site.register(DetectedItem, DetectedItemAdmin)
