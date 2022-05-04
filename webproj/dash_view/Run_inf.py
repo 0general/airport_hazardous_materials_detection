07e0f633ec54d1aa20f35f29082c642f06ef013f
jonghyeon <fish560114@gmail.com>
2022-05-04 14:05:58 +0900

    change display option

--- webproj/dash_view/Run_inf.py
+++ webproj/dash_view/Run_inf.py
@@ -1,12 +1,11 @@
 import os
-os.environ['DISPLAY'] = ':0'
 from glob import glob
 from pathlib import Path
 from sqlite3 import Timestamp
 from tkinter import image_names
 from . import models
 from datetime import datetime
-import pyautogui
+# import pyautogui
 
 def inference(input_root, delete_root, job_num):
     # 원본 동영상 삭제
@@ -15,7 +14,7 @@ def inference(input_root, delete_root, job_num):
 
     # 추론 결과 저장 디렉토리 고유 번호로 생성
     os.system("mkdir /home/ubuntu/result/{}".format(job_num))
-
+    # 추론 모듈 로드
     os.system("/home/ubuntu/yolo/darknet/darknet detector test /home/ubuntu/cfgs/custom_wanda.data /home/ubuntu/cfgs/yolov3_custom_wanda.cfg /home/ubuntu/cfgs/yolov3_custom_last_F1_93.weights")
     count = 0
     for imgs in glob(os.path.join(input_root, "*.jpg")):
@@ -25,8 +24,8 @@ def inference(input_root, delete_root, job_num):
         # argument = "/home/ubuntu/yolo/darknet/darknet detector test /home/ubuntu/cfgs/custom_wanda.data /home/ubuntu/cfgs/yolov3_custom_wanda.cfg /home/ubuntu/cfgs/yolov3_custom_last_F1_93.weights {} thresh=0.4".format(imgs)
         
         # os.system(argument)
-        # os.system(imgs)
-        pyautogui.write(imgs)
+        os.system(imgs)
+        # pyautogui.write(imgs)
 
         # 추론 결과 prediction.jpg 파일명 변경 및 저장
         result_img = "/home/ubuntu/result/{}/inf_{}.jpg".format(job_num, name)
@@ -35,8 +34,8 @@ def inference(input_root, delete_root, job_num):
             id = count,
             timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
             img_name = name,
-            # confidence
-            # item_class
+            # confidence,
+            # item_class,
         )
         noti_data.save()
         # 추론이 끝난 이미지 삭제
